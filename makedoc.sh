c2af0ac12bdecbb8f49e3985a7017bcccadd2651
Jon Chambers <jon@signal.org>
2021-07-21 10:27:00 -0400

    Move Javadoc generation to Maven

--- makedoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf doc/html
-mkdir -p doc/html
-exec javadoc -sourcepath NoiseJava/src \
-    -subpackages com.southernstorm.noise.protocol \
-    -d doc/html \
-    -windowtitle "Noise-Java" \
-    com.southernstorm.noise.protocol

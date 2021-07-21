e6e7db4f37db0a418d6cf72ed93db7413aa1730f
Jon Chambers <jon@signal.org>
2021-07-21 10:17:29 -0400

    Include test vectors as a local resource.
    This eliminates an implicit test dependency on network connectivity.

--- src/test/java/com/southernstorm/noise/tests/UnitVectorTests.java
+++ src/test/java/com/southernstorm/noise/tests/UnitVectorTests.java
@@ -1,19 +1,14 @@
 package com.southernstorm.noise.tests;
 
 import java.io.InputStream;
-import java.net.URL;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UnitVectorTests {
 
-  private static final String testVectorsCommit = "5d0a74760320e5486ced302e36ccad91606aac43";
-
   @Test
   public void testBasicVector() throws Exception {
-    try (InputStream stream = new URL(
-        "https://raw.githubusercontent.com/rweather/noise-c/" + testVectorsCommit
-            + "/tests/vector/noise-c-basic.txt").openStream()) {
+    try (final InputStream stream = getClass().getResourceAsStream("test-vectors.json")) {
       VectorTests vectorTests = new VectorTests();
       vectorTests.processInputStream(stream);
       Assert.assertEquals(vectorTests.getFailed(), 0);
